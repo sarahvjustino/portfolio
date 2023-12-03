75e305c74ec81e16940f5eec943a249e9db25b0f
Sarah <sarahvictoria230@gmail.com>
2023-12-02 22:45:21 -0300

    feat: updated projects area and added scroll up button

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const btnMenu = document.querySelector(".menu-opener");
 const mobileMenu = document.querySelector(".header-menu-mobile");
+const btnUp = document.querySelector("#btnUp");
 
 btnMenu.addEventListener("click", () => {
     btnMenu.classList.toggle("open");
     mobileMenu.classList.toggle("active");
+})
+
+btnUp.addEventListener("click", () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
 })
